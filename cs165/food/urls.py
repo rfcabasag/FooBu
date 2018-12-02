b164773e559b853552e6150c3ee63e6b3c10ac83
KythTerrobias <37520931+KythTerrobias@users.noreply.github.com>
2018-12-02 22:40:12 +0800

    Profile page redesign and Access Control
    Redesigned the profile pages of the owner and costumer.
    Limited the access to the "Add" button in establishment.html. Only owner of the establishment may add

--- cs165/food/urls.py
+++ cs165/food/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('register-owner/', views.register_as_owner, name="register-owner"),
     path('register-customer/', views.register_as_customer, name="register-customer"),
     path('profile/', views.profile, name="profile"),
+    path('fav-<int:est_id>/', views.favorite, name="favorite"),
     path('est-<int:est_id>/', views.establishment, name="establishment"),
-    path('est-<int:est_id>/addfooditem/', views.addfooditem, name="addfooditem")
+    path('est-<int:est_id>/addfooditem/', views.addfooditem, name="addfooditem"),
 ]
--- cs165/food/views.py
+++ cs165/food/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import RegisteredUser, Establishment, FoodItem
+from .models import RegisteredUser, Establishment, FoodItem, Rates, Favorites
 from django.contrib.auth.decorators import login_required
 
 @login_required
@@ -56,15 +57,65 @@ def signup_as(request):
 def profile(request):
     user = request.user
     user_is_owner = RegisteredUser.objects.filter(user=user)[0].is_owner
+    favorites_list = Favorites.objects.select_related('cus').filter(cus__user=user)
+    rated_list = Favorites.objects.select_related('est', 'cus').filter(cus__user=user)
+    rates_list = []
 
-    return render(request,'food/profile.html',{'an_owner':user_is_owner})
+    owned = Establishment.objects.filter(owner__user=user)
+
+    for r in rated_list:
+        raters = len(Rates.objects.filter(est=r.est))
+        rate = 0
+        if raters !=0:
+            for i in Rates.objects.filter(est=r.est):
+                rate = rate + i.rating
+            rate = rate/raters
+        rates_list.append(rate)
+
+    return render(request,'food/profile.html',{'an_owner':user_is_owner, 'favorites_list':favorites_list, 'rates_list':rates_list, 'user':user, 'rated_list':rated_list, 'owned':owned})
 
 @login_required
 def establishment(request,est_id):
     x = Establishment.objects.filter(id=est_id)[0]
     location = x.street + " " + x.area
+
+    user = request.user
+    is_owner = RegisteredUser.objects.filter(user=user)[0].is_owner
+    owns = (user == x.owner.user)
+    
+
+    raters = len(Rates.objects.filter(est=x))
+    rate = 0
+    if raters !=0:
+        for i in Rates.objects.filter(est=x):
+            rate = rate + i.rating
+        rate = rate/raters
+        
+    favs = len(Favorites.objects.filter(est=x))
     fooditems = FoodItem.objects.filter(est=x)
-    return render(request,'food/establishment.html', {'est':x, 'loc':location, 'fooditems':fooditems})
+
+    return render(request,'food/establishment.html', {'est':x, 'loc':location, 'owner':is_owner, 'favorite':favs, 'rate':rate, 'fooditems':fooditems, 'owns':owns})
+
+@login_required
+def favorite(request,est_id):
+    user = request.user
+    curr_owner = RegisteredUser.objects.filter(user=user)[0]
+    is_owner = curr_owner.is_owner
+
+
+    if is_owner:
+        return redirect('food-home')
+
+    est = Establishment.objects.filter(id=est_id)[0]    
+    fav = len(Favorites.objects.filter(cus=curr_owner,est=est))
+    if fav==0:
+        newFav = Favorites(cus=curr_owner,est=est)
+        newFav.save()
+    else:
+        Favorites.objects.filter(cus=curr_owner,est=est)[0].delete()
+    
+    return redirect('establishment',est_id=est_id)
+    
 
 @login_required
 def addfooditem(request,est_id):
